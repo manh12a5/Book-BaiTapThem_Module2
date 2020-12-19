a58ffe72ced786a5fbc4ac1e2be0f39f726ef1e2
manh12a5 <manh12a5@gmail.com>
2020-12-19 22:17:52 +0700

    'Module2'

---
+++ src/Book/ManagerBook.java
@@ -0,0 +1,110 @@
+package Book;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManagerBook {
+
+    //Danh sách thuộc tính
+    int numberProgrammingBook;
+    int numberFictionBook;
+    double totalProgrammingBook = 0;
+    double totalFictionBook = 0;
+
+    //Tạo mảng ProgrammingBook và FictionBook
+    ArrayList<ProgrammingBook> arrProgrammingBook = new ArrayList<>();
+    ArrayList<FictionBook> arrFictionBook = new ArrayList<>();
+
+    //Danh sách phướng thức
+    public ManagerBook() {
+    }
+
+    public void menuBook() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số sách Programming Book: ");
+        numberProgrammingBook = scanner.nextInt();
+        System.out.println("Nhập số sách Fiction Book: ");
+        numberFictionBook = scanner.nextInt();
+    }
+
+    public void informationProgrammingBook() {
+        System.out.println("Nhập thông tin Programming Book");
+        for (int i = 0; i < numberProgrammingBook; i++) {
+            System.out.println("Số thứ tự " + (i + 1));
+            ProgrammingBook programmingBook = new ProgrammingBook();
+            programmingBook.addNewBook();
+            arrProgrammingBook.add(programmingBook);
+        }
+    }
+
+    public void informationFictionBook() {
+        System.out.println("Nhập thông tin Fiction Book");
+        for (int i = 0; i < numberFictionBook; i++) {
+            System.out.println("Số thứ tự " + (i + 1));
+            FictionBook fictionBook = new FictionBook();
+            fictionBook.addNewBook();
+            arrFictionBook.add(fictionBook);
+        }
+    }
+
+    public void listProgrammingBook() {
+        System.out.println("Thông tin Programming Book");
+        for (int i = 0; i < arrProgrammingBook.size(); i++) {
+            System.out.println(arrProgrammingBook.get(i).toString());
+        }
+    }
+
+    public void listFictionBook() {
+        for (int i = 0; i < arrFictionBook.size(); i++) {
+            System.out.println(arrFictionBook.get(i).toString());
+        }
+    }
+
+    public void getTotalProgrammingBook() {
+        for (int i = 0; i < arrProgrammingBook.size(); i++) {
+            totalProgrammingBook += arrProgrammingBook.get(i).getPrice();
+        }
+        System.out.println("Tổng tiền Programming Book: " + totalProgrammingBook);
+    }
+
+    public void getTotalFictionBook() {
+        for (int i = 0; i < arrFictionBook.size(); i++) {
+            totalFictionBook += arrFictionBook.get(i).getPrice();
+        }
+        System.out.println("Tông tiền Fiction Book: " + totalFictionBook);
+    }
+
+    public double getTotalBook() {
+        return totalProgrammingBook + totalFictionBook;
+    }
+
+    public int checkCount() {
+        int count = 0;
+        for (int i = 0; i < arrProgrammingBook.size(); i++) {
+            if (arrProgrammingBook.get(i).getLanguage().contains("Java")
+                    || arrProgrammingBook.get(i).getLanguage().contains("java")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Gộp mảng
+    //Tìm kiếm giá sách
+    public void searchPrice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập giá sách cần tìm: ");
+        int search = scanner.nextInt();
+        ArrayList<Book> arrayBook = new ArrayList<>();
+        arrayBook.addAll(arrProgrammingBook);
+        arrayBook.addAll(arrFictionBook);
+        for (int i = 0; i < arrayBook.size(); i++) {
+            if (search == arrayBook.get(i).getPrice()) {
+                System.out.println(arrayBook.get(i));
+            } else {
+                System.out.println("Not Found");
+            }
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,60 @@
-import Book.Book;
-import Book.FictionBook;
-import Book.ProgrammingBook;
+import Book.ManagerBook;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<ProgrammingBook> arrProgrammingBook = new ArrayList<>();
-        ArrayList<FictionBook> arrFictionBook = new ArrayList<>();
+        ManagerBook managerBook = new ManagerBook();
 
-        int numberProgrammingBook;
-        int numberFictionBook;
-        double totalProgrammingBook = 0;
-        double totalFictionBook = 0;
-        double totalBook;
+        //Điền thông tin Book
+        managerBook.menuBook();
+        managerBook.informationProgrammingBook();
+        managerBook.informationFictionBook();
 
-        System.out.println("Nhập số sách Programming Book: ");
-        numberProgrammingBook = scanner.nextInt();
-        System.out.println("Nhập số sách Fiction Book: ");
-        numberFictionBook = scanner.nextInt();
-
-        //Nhập thông tin
-        System.out.println("Nhập thông tin Programming Book");
-        for (int i = 0; i < numberProgrammingBook; i++) {
-            System.out.println("Số thứ tự " + (i + 1));
-            ProgrammingBook programmingBook = new ProgrammingBook();
-            programmingBook.addNewBook();
-            arrProgrammingBook.add(programmingBook);
-        }
-
-        System.out.println("Nhập thông tin Fiction Book");
-        for (int i = 0; i < numberFictionBook; i++) {
-            System.out.println("Số thứ tự " + (i + 1));
-            FictionBook fictionBook = new FictionBook();
-            fictionBook.addNewBook();
-            arrFictionBook.add(fictionBook);
-        }
-
-        System.out.println("\n");
-
-        //Programming Book
-        System.out.println("Thông tin Programming Book");
-        for (int i = 0; i < arrProgrammingBook.size(); i++) {
-            System.out.println(arrProgrammingBook.get(i).toString());
-        }
-        //Tông tiền Programming Book
-        for (int i = 0; i < arrProgrammingBook.size(); i++) {
-            totalProgrammingBook += arrProgrammingBook.get(i).getPrice();
-        }
-        System.out.println("Tông tiền Programming Book: " + totalProgrammingBook);
-
-        System.out.println("\n");
-
-        //Fiction Book
-        System.out.println("Thông tin Fiction Book");
-        for (int i = 0; i < arrFictionBook.size(); i++) {
-            System.out.println(arrFictionBook.get(i).toString());
-        }
-        //Tông tiền Fiction Book
-        for (int i = 0; i < arrFictionBook.size(); i++) {
-            totalFictionBook += arrFictionBook.get(i).getPrice();
-        }
-        System.out.println("Tông tiền Fiction Book: " + totalFictionBook);
-
-        System.out.println("\n");
-
-        //Tổng tiền
-        totalBook = totalFictionBook + totalProgrammingBook;
-        System.out.println("Tông tiền phải chi là: " + totalBook);
-
-        System.out.println("\n");
-
-        //Đếm số sách có language là Java
-        int count = 0;
-        for (int i = 0; i < arrProgrammingBook.size(); i++) {
-            if (arrProgrammingBook.get(i).getLanguage().contains("Java")
-                    || arrProgrammingBook.get(i).getLanguage().contains("java"))
-                count++;
-        }
-        System.out.println("Số sách có ngôn ngữ Java: " + count);
-
-        System.out.println("\n");
-
-        //Gộp 2 mảng vào 1 mảng
-        ArrayList<Book> arrAllBooks = new ArrayList<>();
-        arrAllBooks.addAll(arrProgrammingBook);
-        arrAllBooks.addAll(arrFictionBook);
-
-        //Tìm kiếm giá cuốn sách
-        System.out.println("Nhập giá cần tìm: ");
-        int checkPrice;
-        checkPrice = scanner.nextInt();
-        for (int i = 0; i < arrAllBooks.size(); i++) {
-            if ( checkPrice == arrAllBooks.get(i).getPrice()) {
-                System.out.println("Thông tin sách cần tìm: " + arrAllBooks.get(i));
-            } else {
-                System.out.println("Không có kết quả");
+        //Hiển thị thông tin Book đã nhập
+        //Tính tổng các loại sách
+        //Tìm kiếm
+        int number;
+        do {
+            System.out.println("1. Thông tin Programming Book");
+            System.out.println("2. Thông tin Fiction Book");
+            System.out.println("3. Tổng tiền Programming Book");
+            System.out.println("4. Tổng tiền Fiction Book");
+            System.out.println("5. Tổng tiền");
+            System.out.println("6. Tìm kiếm ngôn ngữ Java trong ProgrammingBook");
+            System.out.println("7. Tìm kiếm giá tiền");
+            System.out.println("0. Exit");
+            System.out.println("Choice Number: ");
+            number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    managerBook.listProgrammingBook();
+                    break;
+                case 2:
+                    managerBook.listFictionBook();
+                    break;
+                case 3:
+                    managerBook.getTotalProgrammingBook();
+                    break;
+                case 4:
+                    managerBook.getTotalFictionBook();
+                    break;
+                case 5:
+                    System.out.println("Tông tiền: " + managerBook.getTotalBook());
+                    break;
+                case 6:
+                    System.out.println("Số lượng sách có ngôn ngữ Java trong Programming Book: " +
+                            managerBook.checkCount());
+                    break;
+                case 7:
+                    managerBook.searchPrice();
+                    break;
             }
-        }
-
+            System.out.println("\n");
+        } while (number != 0);
+        System.exit(0);
 
     }
-
 }
 
